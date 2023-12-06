446b5013cd80512b48a1a7d71337d1cb652924c2
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-06 22:28:05 +0530

    modified the program to create a contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,10 +1,89 @@
 package addressBook;
 
+/*
+Getters and Setters are used to set and get values of the contacts in the Address Book
+Address Book consist of Contact details :First Name,Last Name,Address,City,State,Zip,Phone Numberand Email
+ UC_1 : Ability to create a Contacts in Address
+ */
 public class AddressBook {
     public static void display(){
         System.out.println("Welcome to Address Book Program");
     }
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
 
+    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     public static void main(String[] args) {
         display();
     }
